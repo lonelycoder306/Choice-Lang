ac291025156a9e8630e73c6cb202b2e8df9602ff
Ali Hamza <ali.mhamza06@gmail.com>
2025-11-28 19:27:09 +0400

    Extend type aliases and add externally linked variables for the interpreter.

--- include/common.h
+++ include/common.h
@@ -1,12 +1,36 @@
 #pragma once
+#include "object.h"
 #include <cstdint>
+#include <memory>
+#include <string>
 #include <vector>
+// #define ALT
 
 class Token;
-using vT = std::vector<Token>;
+using vT    	= std::vector<Token>;
+using vByte 	= std::vector<uint8_t>;
+using vObj  	= std::vector<Object::BaseUP>;
 
-constexpr bool timeRun = false;
-constexpr bool dumpTokens = true;
-constexpr bool dumpBytecode = false;
-constexpr bool executeCode = false;
+using Byte  	= int8_t;
+using Short 	= int16_t;
+using Long  	= int32_t;
+
+using UByte     = uint8_t;
+using UShort    = uint16_t;
+using ULong     = uint32_t;
+
+using i8        = int8_t;
+using i16       = int16_t;
+using i32       = int32_t;
+using i64       = int64_t;
+
+using ui8       = uint8_t;
+using ui16      = uint16_t;
+using ui32      = uint32_t;
+using ui64      = uint64_t;
+
 constexpr int TAB_SIZE = 4;
+// Whether we're running an externally loaded
+// program or not.
+extern bool external;
+extern std::string file;
