7ab20cef9ff7a00441f52071af25c2c8541acce5
Ali Hamza <ali.mhamza06@gmail.com>
2026-01-13 01:17:25 +0400

    Correct token type-checking macros.

--- include/token.h
+++ include/token.h
@@ -176,8 +176,8 @@ class Token
 		friend class VM;
 };
 
-#define IS_LITERAL(type)	((TOK_NUM <= type) && (type <= TOK_NULL))
-#define IS_TYPE(type)		((TOK_INT <= type) && (type <= TOK_ANY))
+#define IS_LITERAL(type)	((TOK_NUM <= (type)) && ((type) <= TOK_NULL))
+#define IS_TYPE(type)		((TOK_INT <= (type)) && ((type) <= TOK_ANY))
 #define IS_ASSIGN(type) \
 	(((type) == TOK_EQUAL) \
 	|| (((type) >= TOK_PLUS_EQ) && ((type) <= TOK_RSHIFT_EQ)))
